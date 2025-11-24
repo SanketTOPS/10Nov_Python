8cf0097495e6a9343f2b139e8f1d3a16732a02d8
SanketTops <sanket.tops@gmail.com>
2025-11-24 10:11:07 +0530

    String

---
+++ Assignments/Module-1/pass.py
@@ -0,0 +1,10 @@
+"""for i in range(1,11):
+    #statement
+    pass"""
+    
+
+a=5
+b=76
+
+if a<b:
+    pass
---
+++ Assignments/Module-1/str_example.py
@@ -0,0 +1,38 @@
+mystr="this is python!"
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4]) #slice - range
+print(mystr[5:])
+print(mystr[:4])"""
+
+"""print(mystr)
+print(len(mystr))"""
+
+# String Methods
+#print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace("This","That"))
+#print(mystr.replace('T','Y'))
+
+# ------------------------------------------- #
+#print(mystr)
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count(" "))
+#print(mystr.startswith("That"))
+#print(mystr.endswith("!"))
+#print(mystr.find("P"))
+#print(mystr.index("PYTHON"))
+
+# ------------------------------------ #
+print(mystr)
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.isupper())
+#print(mystr.islower())
+
+print(mystr.title())

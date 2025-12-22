eea2046010441c8b57fa5a2ff753598c20e40796
SanketTops <sanket.tops@gmail.com>
2025-12-22 10:50:59 +0530

    Exceptions

---
+++ Assignments/Module-3_Exceptions/exce_example.py
@@ -0,0 +1,8 @@
+a=45
+b=76
+
+print("Sum:",A+b)
+
+x=65
+y=88
+print("Mul:",x*y)
---
+++ Assignments/Module-3_Exceptions/try_except.py
@@ -0,0 +1,11 @@
+a=45
+b=76
+
+try:
+    print("Sum:",A+b)
+except:
+    print("Error!")
+
+x=65
+y=88
+print("Mul:",x*y)
